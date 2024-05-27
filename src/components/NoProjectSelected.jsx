a4d0ca2cc8699854d582e4dd4485a11b8fab4452
Rohit Singh <rohitsinghpb4@gmail.com>
2024-05-27 21:49:42 +0530

    NoProjectSelected (component) added

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,12 @@
+export default function NoProjectSelected() {
+    return (
+        <div>
+            <img />
+            <h2>No Project Selected</h2>
+            <p>Select a project or get started with a new one</p>
+            <p>
+                <button>Create new project</button>
+            </p>
+        </div>
+    );
+}
