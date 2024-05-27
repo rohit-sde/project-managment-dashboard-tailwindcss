bf1f75e7b25d2b4ac7d21ecf31b8f36c66c8192b
Rohit Singh <rohitsinghpb4@gmail.com>
2024-05-27 17:24:40 +0530

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ProjectSidebar from "./components/ProjectsSidebar.jsx";
+// import { useState } from 'react'
+// import './App.css'
+
+function App() {
+    return (
+        <main className="h-screen my-8">
+            <ProjectSidebar />
+        </main>
+    );
+}
+
+export default App;
---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectSidebar() {
+    return (
+        <aside className="w-1/3 px-8 py16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+            <h2>Your Projects</h2>
+            <div>
+                <button>+ Add Project</button>
+            </div>
+            <ul></ul>
+        </aside>
+    );
+}
