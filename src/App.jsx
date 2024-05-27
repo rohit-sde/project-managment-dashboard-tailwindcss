9833ef8ceaa38531de9c6de24442aa8dfae7fa21
Rohit Singh <rohitsinghpb4@gmail.com>
2024-05-27 21:48:22 +0530

    Tailwind css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ProjectSidebar from "./components/ProjectsSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
 // import { useState } from 'react'
 // import './App.css'
 
 function App() {
     return (
-        <main className="h-screen my-8">
+        <main className="h-screen my-8 pt-7 flex gap-8">
             <ProjectSidebar />
+            <NewProject />
         </main>
     );
 }
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,9 +1,13 @@
 export default function ProjectSidebar() {
     return (
         <aside className="w-1/3 px-8 py16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
-            <h2>Your Projects</h2>
+            <h2 className="mb-8 pt-20 font-bold uppercase md:text-xl text-stone-200">
+                Your Projects
+            </h2>
             <div>
-                <button>+ Add Project</button>
+                <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100 ">
+                    + Add Project
+                </button>
             </div>
             <ul></ul>
         </aside>
